b141a0c4df497e38fc186ca3451e63f328bb19fc
aecci2023 <developeraecci@gmail.com>
2024-08-24 20:30:46 +0530

    homepage done!

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import bppmic from '../assets/bppmic.png';
 import bpplogo from '../assets/bpplogo.png';
 import { ChevronRight } from "lucide-react";
 import { toast } from "react-hot-toast";
+import bppflag from "../assets/bppflag.png"
 import axios from "axios";
 import { getURLbyEndPointV2 } from "@/api";
 
@@ -140,7 +141,8 @@ export function HomePage() {
           </h2>
         </div>
         <div className="mt-10 bg-white rounded-lg p-8 max-w-xl w-full shadow-lg flex-shrink-0">
-          <h3 className="text-xl font-bold text-gray-800 mb-4">Enroll Now</h3>
+          <h3 className="text-2xl font-bold text-gray-800">Enroll Now</h3>
+          <h6 className="text-sm font-semibold text-gray-600 mb-4">Fill all the details as per Voter Id *</h6>
           <form onSubmit={handleFormSubmit} className="space-y-4">
             <div className="flex flex-row space-x-4 lg:flex-row lg:space-x-4">
               <div className="flex-1">
@@ -260,7 +262,7 @@ export function HomePage() {
 
             <button
               type="submit"
-              className="w-full py-3 bg-pink-500 text-white rounded-md hover:bg-pink-600 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
+              className="w-full py-3 bg-blue-600 text-white rounded-md hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
               disabled={registerFormSuccess}
               >
                 {!registerFormSuccess ? "Enroll Now" : "Processing..."}
@@ -269,51 +271,84 @@ export function HomePage() {
         </div>
       </div>
     </div>
-    <div className="container h-3/5">
-    <div className="my-40">
+  
+            <div className="relative py-28" style={{background: "#D3E6E0"}}>
+  <div>
+    <div className="relative mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="lg:flex lg:items-center lg:justify-between">
+        <div className="lg:w-1/2">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            Our Vision
+          </h1>
+          <p className="mt-3 text-lg text-gray-600">
+            BPP’s vision is: "Empowering communities through decentralized solutions while driving combined progress."
+          </p>
+          <p className="mt-3 text-lg text-gray-600">
+            Our vision is to create a society where local initiatives prosper with autonomy and flexibility, yet contribute to a progressive national impact. By understanding the strength of localized efforts and aligning them with a central mission,
+          </p>
+          <p className="mt-3 text-lg text-gray-600">
+            we strive to achieve sustainable development, peace, and prosperity on the national level."
+          </p>
+        </div>
+        <div className="mt-10 lg:mt-0 lg:w-1/2 lg:pl-10">
+          <div className="relative overflow-hidden rounded-lg">
+            <img
+              alt="bppflag"
+              src={bppflag}
+              className="h-full w-full object-cover object-center"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
+<div className="container h-3/5">
+    <div className="my-40">
             <h1 className="text-4xl text-center font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Our Vision
+              Our Mission
             </h1>
             <p className="mt-3 text-lg  container text-gray-600">
-            BPP’s vision is: "Empowering communities through decentralized solutions while driving combined progress.</p>
+            BPP's mission is: "Our mission is to empower individuals by leveraging a decentralized society, giving the common man a central role in shaping the nation. </p>
             
-            <p className="mt-3 text-lg  container text-gray-600"> Our vision is to create a society where local initiatives prosper with autonomy and flexibility, yet contribute to a progressive national impact. By understanding the strength of localized efforts and aligning them with a central mission, 
+            <p className="mt-3 text-lg  container text-gray-600">We aim to create party's platform where educated representatives from different parts of the community can connect, share ideas, and collaborate on services. By ensuring decisions are made at the grassroots level, we support and drive the overall growth of the nation. 
             </p>
               
-            <p className="mt-3 text-lg  container text-gray-600">we strive to achieve sustainable development, peace, and prosperity on national level."
+            <p className="mt-3 text-lg  container text-gray-600">While planning and implementation are decentralized, we focus on achieving centralized outcomes and sustainable growth."
             </p>
             </div>
             </div>
 
-    <footer className="w-full pb-8 pt-12 md:mt-16 border-t">
+    <footer className="w-full bg-slate-900 pb-8 pt-12 md:mt-16 border-t">
         <div className="mx-auto flex max-w-6xl flex-col items-start space-x-8 md:flex-row">
-          <div className="w-full px-4 md:w-1/2 lg:px-0">
-            <h1 className="max-w-sm text-3xl font-bold">Subscribe to our Newsletter</h1>
+          <div className="w-full px-6 md:w-1/2 lg:px-0">
+            <h1 className="max-w-sm text-3xl text-white font-bold">Subscribe to our Newsletter</h1>
             <form action="" className="mt-4 inline-flex w-full items-center md:w-3/4">
-              <input
-                className="flex h-10 w-full rounded-md border border-black/20 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
-                type="email"
-                placeholder="Email"
-              ></input>
+            <input
+  className="flex h-10 w-full rounded-md border border-white/60 bg-transparent px-3 py-2 text-sm placeholder-white focus:outline-none focus:ring-1 focus:ring-white focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 text-white"
+  type="email"
+  placeholder="Email"
+/>
+
               <button
                 type="button"
-                className="ml-4 rounded-full bg-black px-3 py-3 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                className="ml-4 rounded-full bg-white px-3 py-3 text-sm font-semibold text-white shadow-sm hover:bg-white/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
                 onClick={()=> alert("thank you for subscribe!")}
               >
-                <ChevronRight className="h-4 w-4" />
+                <ChevronRight className="h-4 w-4 text-black" />
               </button>
             </form>
           </div>
           <div className="mt-8 grid w-4/5 gap-6 md:mt-0 lg:w-3/4 ">
-          <p className="mb-6 text-lg font-semibold text-gray-700">Please be advised that Bharatiya Popular Party does not hold any official accounts on social media platforms.
+          <p className="mb-6 text-lg font-semibold text-white">Please be advised that Bharatiya Popular Party does not hold any official accounts on social media platforms.
           </p>
-          <p className="mb-6 text-lg font-semibold text-gray-700 " >
+          <p className="mb-6 text-lg font-semibold text-white" >
 
 Our organization is not present on any social media network, including but not limited to Facebook, Twitter, Instagram, LinkedIn, and others. Reporting Unauthorized Accounts
 
           </p>
-          <p className="mb-6 text-lg font-semibold text-gray-700 ">
+          <p className="mb-6 text-lg font-semibold text-white">
 If you encounter any social media accounts or profiles that claim to represent Bhartiya Popular Party or use our name, logo, or any other intellectual property, please notify us immediately. These accounts are unauthorized and not affiliated with our organization. To report any such accounts, please contact us through email.</p>
           
           </div>
@@ -326,7 +361,7 @@ If you encounter any social media accounts or profiles that claim to represent B
             className="flex gap-3 font-bold text-black-700 items-center"
             >
              <img src={bppmic} width={25} height={25}/>
-              <span className='poppins-regular font-black text-md lg:text-xl'>BHARATIYA POPULAR PARTY</span>
+              <span className='poppins-regular text-white font-black text-md lg:text-xl'>BHARATIYA POPULAR PARTY</span>
             </a>
           </div>
           <div className="mt-4 md:mt-0">
